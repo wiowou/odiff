11933b37a2432957bdc3e3b7012d28538fb7f59a
abc <bk@pacman.localdomain>
2021-11-03 23:37:23 -0400

    amended types

--- src/diff.ts
+++ src/diff.ts
@@ -1,14 +1,5 @@
 import * as R from 'ramda';
 
-// const complementary = (lhs, rhs) =>
-//   R.pipe(
-//     R.keys,
-//     R.difference(R.keys(lhs)),
-//     R.map((k) => [k, undefined]),
-//     Object.fromEntries,
-//     R.merge(rhs)
-//   )(rhs);
-
 //predicates
 const predAdded = ([prime, obj]) => (prime ? [0, 0] : [prime, obj]);
 const predRemoved = ([prime, obj]) => (obj ? [0, 0] : [prime, obj]);
@@ -16,11 +7,11 @@ const predUpdated = ([prime, obj]) => (!obj || !prime ? [0, 0] : [prime, obj]);
 const predDiff = ([prime, obj]) => [prime, obj];
 
 const _diff = (predicate): ((a: [any, any]) => object) => {
-  return function _rec([p, o]: [any, any]): object {
+  return function _rec([p, o]: [any, any]) {
     if (R.equals(...predicate([p, o]))) return {};
     if (R.isEmpty(R.keys(p))) return o;
-    const keys = R.union(R.keys(o), R.keys(p));
-    return keys.reduce((acc, key) => {
+    //const keys = R.union(R.keys(o), R.keys(p));
+    return R.union(R.keys(o), R.keys(p)).reduce((acc, key) => {
       const result = _rec(predicate([p[key], o[key]]));
       if (R.isEmpty(result)) return acc;
       return { ...acc, [key]: result };
@@ -28,7 +19,7 @@ const _diff = (predicate): ((a: [any, any]) => object) => {
   };
 };
 
-export const addedDiff = (prime, obj): object => _diff(predAdded)([prime, obj]);
-export const removedDiff = (prime, obj): object => _diff(predRemoved)([prime, obj]);
-export const updatedDiff = (prime, obj): object => _diff(predUpdated)([prime, obj]);
-export const diff = (prime, obj): object => _diff(predDiff)([prime, obj]);
+export const addedDiff = (prime, obj) => _diff(predAdded)([prime, obj]);
+export const removedDiff = (prime, obj) => _diff(predRemoved)([prime, obj]);
+export const updatedDiff = (prime, obj) => _diff(predUpdated)([prime, obj]);
+export const diff = (prime, obj) => _diff(predDiff)([prime, obj]);
