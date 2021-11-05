1241cca46d4122810f6b3148f04f654ed5ca2025
abc <bk@pacman.localdomain>
2021-11-04 23:02:29 -0400

    passing tests for diff

--- src/diff.ts
+++ src/diff.ts
@@ -1,26 +1,39 @@
 import * as R from 'ramda';
 
-const ignore = [0, 0];
+// const complementary = (lhs, rhs) =>
+//   R.pipe(
+//     R.keys,
+//     R.difference(R.keys(lhs)),
+//     R.map((k) => [k, undefined]),
+//     Object.fromEntries,
+//     R.merge(rhs)
+//   )(rhs);
+const isUndefined = (x: any) => x === undefined && typeof x == 'undefined';
+const hasKeys = R.pipe(R.keys, R.isEmpty, R.not);
+const getKey = R.curry((key: string, x: any) => R.view(R.lensProp(key), x));
 //predicates
-const predAdded = ([prime, obj]) => (prime ? ignore : [prime, obj]);
-const predRemoved = ([prime, obj]) => (obj ? ignore : [prime, obj]);
-const predUpdated = ([prime, obj]) => (!obj || !prime ? ignore : [prime, obj]);
-const predDiff = ([prime, obj]) => [prime, obj];
+export const SKIP = [0, 0];
+const addedOnly = ([primus, sec]) => (primus ? SKIP : [primus, sec]);
+const removedOnly = ([primus, sec]) => (sec ? SKIP : [primus, sec]);
+const updatedOnly = ([primus, sec]) => (!sec || !primus ? SKIP : [primus, sec]);
+const all = ([primus, sec]) => [primus, sec];
 
-const _diff = (predicate): ((a: [any, any]) => object) => {
-  return function _rec([p, o]: [any, any]) {
-    if (R.equals(...predicate([p, o]))) return {};
-    if (R.isEmpty(R.keys(p))) return o;
-    //const keys = R.union(R.keys(o), R.keys(p));
-    return R.union(R.keys(o), R.keys(p)).reduce((acc, key) => {
-      const result = _rec(predicate([p[key], o[key]]));
-      if (R.isEmpty(result)) return acc;
-      return { ...acc, [key]: result };
-    }, {});
-  };
-};
+export const diffWith = R.curry(
+  (predicate: (a: [any, any]) => [any, any], primus: any, sec: any) => {
+    function _diff([p, s]: [any, any]) {
+      if (R.equals(...predicate([p, s]))) return {};
+      if ((!hasKeys(p) && !hasKeys(s)) || isUndefined(s)) return s;
+      const keys = R.union(R.keys(s), R.keys(p));
+      return keys.reduce((acc: object, key: string) => {
+        const result = _diff([getKey(key, p), getKey(key, s)]);
+        return R.isEmpty(result) ? acc : { ...acc, [key]: result };
+      }, {});
+    }
+    return _diff([primus, sec]);
+  }
+);
 
-export const addedDiff = (prime, obj) => _diff(predAdded)([prime, obj]);
-export const removedDiff = (prime, obj) => _diff(predRemoved)([prime, obj]);
-export const updatedDiff = (prime, obj) => _diff(predUpdated)([prime, obj]);
-export const diff = (prime, obj) => _diff(predDiff)([prime, obj]);
+export const addedDiff = diffWith(addedOnly);
+export const removedDiff = diffWith(removedOnly);
+export const updatedDiff = diffWith(updatedOnly);
+export const diff = diffWith(all);
